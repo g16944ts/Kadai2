ad151cc243ead476c8501287c9a2bda12a91fa32
g16944ts <g16944ts@gm.tsuda.ac.jp>
2018-04-12 14:10:43 +0900

    Third Commit

--- Kadai1/src/Sample.java
+++ Kadai1/src/Sample.java
@@ -1,6 +1,6 @@
 
 public class Sample {
  public static void main(String[] args){
-	 System.out.println("課題がかなり多めでグループワークもたくさんあるということなので、根気よく頑張りたいと思います！");
+	 System.out.println("課題がかなり多めでグループワークもたくさんあるということなので、根気よく精一杯頑張りたいと思います！");
  }
 }
